551ea39a8c6c4f7c38060c933ccf5966a667e0ce
Naresh Bhatia <naresh.bhatia@getcruise.com>
2023-08-16 09:24:49 -0400

    chore: add storybook

---
+++ apps/next13-rendering-concepts-storybook/.storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const config: StorybookConfig = {
+  stories: [
+    '../../../packages/*/src/**/*.stories.mdx',
+    '../../../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../apps/*/src/**/*.stories.mdx',
+    '../../../apps/*/src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+    });
+  },
+};
+
+export default config;
---
+++ apps/next13-rendering-concepts-storybook/README.md
@@ -0,0 +1,4 @@
+# Next13 Rendering Concepts Storybook
+
+This is a [Storybook](https://storybook.js.org/) bootstrapped with
+[Code Shaper](https://code-shaper.dev).
